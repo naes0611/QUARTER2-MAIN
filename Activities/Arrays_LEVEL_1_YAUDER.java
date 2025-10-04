c2358da90c3d3aad05580bea980e2fa0e71ed470
naes0611 <seanyauder1126@gmail.com>
2025-10-04 08:22:34 +0800

    Update Arrays_LEVEL_1_YAUDER.java
    minor changes

--- Activities/Arrays_LEVEL_1_YAUDER.java
+++ Activities/Arrays_LEVEL_1_YAUDER.java
@@ -7,11 +7,10 @@ import java.util.*;
 public class Arrays_LEVEL_1_YAUDER {
     static Scanner array = new Scanner(System.in);
     public static void main(String[] args) {
-        int size;
-        int total;
+        String[] things;
         System.out.print("Enter Array size: ");
-        size = array.nextInt();
-        String[] things = new String[size];
+        int size = array.nextInt();
+        things = new String[size];
         System.out.print("Input "+size+" Things: ");
         for(int i=0; i<size;i++){
         things[i]= array.next();
@@ -20,8 +19,8 @@ public class Arrays_LEVEL_1_YAUDER {
         System.out.println(things.length +" Things: ");
         for (int i=0; i<size;i++)
         {
-            int howMany = i +1;
-            System.out.println(howMany+" "+things[i]);
+            System.out.println( i+1 +" "+things[i]);
         }   
     }
 }
+
