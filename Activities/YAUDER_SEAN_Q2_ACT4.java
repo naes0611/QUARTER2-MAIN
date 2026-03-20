dcf90bbe2d4b62e19ae1ce5f1333db4cecc2f54c
naes0611 <230276362+naes0611@users.noreply.github.com>
2026-03-20 08:59:38 +0800

    Update YAUDER_SEAN_Q2_ACT4.java

--- Activities/YAUDER_SEAN_Q2_ACT4.java
+++ Activities/YAUDER_SEAN_Q2_ACT4.java
@@ -9,25 +9,20 @@ import java.util.Scanner;
  */
 public class YAUDER_SEAN_Q2_ACT4 {
     public static void main(String[] args) {
-        Scanner math = new Scanner(System.in);
+        try (Scanner math = new Scanner(System.in)) {
             System.out.println("=======================");
             System.out.println("  Sum of Even Numbers  ");
             System.out.println("=======================");
             System.out.println("");
             System.out.print("Enter a number: ");
             int number = math.nextInt();
-            int i = 2;
             int sum = 0;
-            while (i<=number) {
-                sum = sum + i;
-                i+=2;
-            } System.out.println("The sum of even numbers from 1"+" to " +number +": "+sum);
-//            for (int i = 2;i <= number; i+=2){
-//                int sum = 0;
-//                sum+=i
-//                System.out.println(sum);
-//            }
-            math.close();
+            for (int j = 0; j <= number; j++) {
+                if (j % 2 == 0) { // change to 1 to find sum of odd numbers
+                    sum += j;
+                }
+            }
+            System.out.println("The sum of even numbers from 1"+" to " +number +": "+sum);
+        }
     }
 }
-//    System.out.println("The sum of even numbers from 1"+" to " +number +": "sum);
